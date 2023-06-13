1e37fac39deba239b5899bfa5ea32fa254406d1e
kseb49 <kseb49@gmail.com>
2023-06-13 16:13:16 +0200

    blog page

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -8,4 +8,7 @@ class BlogController extends Controller{
     public function index(){
        $this->twig->display('blog.twig');
     }
+    public function single(array $params){
+      $this->twig->display('post.twig');
+    }
 }
