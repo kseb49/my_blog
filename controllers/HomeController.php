ec81183a2bcfa91f118cdb855aebd2c30ed78f23
kseb49 <kseb49@gmail.com>
2023-06-21 17:08:23 +0200

    update the Blogmodel call in the index method(remove the parameter needed)

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -8,7 +8,7 @@ use models\BlogModel;
 class HomeController extends Controller
 {
     public function index(){
-        $posts = new BlogModel($this->connect());
+        $posts = new BlogModel();
         $posts = $posts->home();
         $this->twig->display('home.twig',['posts'=>$posts]);
     }
