ea36249f858442b8c95d15891d103fe132b893ee
kseb49 <kseb49@gmail.com>
2023-06-21 17:23:59 +0200

    update to use the db connection from the parent and comment the method

--- models/CommentModel.php
+++ models/CommentModel.php
@@ -2,20 +2,18 @@
 
 namespace models;
 
-use PDO;
+use core\Model;
 
-class CommentModel
+class CommentModel extends Model
 {
-    private PDO $db;
-
-    public function __construct(PDO $db)
-    {   
-        $this->db = $db;
-        
-    }
-
+    /**
+     * Get all the comments bounded to a posts
+     *
+     * @param string $id
+     * @return array
+     */
     public function fetch (string $id):array{
-        $request = $this->db->prepare(
+        $request = $this->connect()->prepare(
         'SELECT * FROM comments left join users on users.id = comments.users_id where comments.posts_id = ? and comments.status = 1 order by comments._date desc');
         $request->execute([$id]);
         $resp = $request->fetchAll();
