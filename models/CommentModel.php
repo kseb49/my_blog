dc0d76749e55c3bd48abefc92be67cd22b41ab95
kseb49 <kseb49@gmail.com>
2023-06-21 16:35:10 +0200

    add a order by in the query of the fetch method

--- models/CommentModel.php
+++ models/CommentModel.php
@@ -16,7 +16,7 @@ class CommentModel
 
     public function fetch (string $id):array{
         $request = $this->db->prepare(
-        'SELECT * FROM comments left join users on users.id = comments.users_id where comments.posts_id = ? and comments.status = 1');
+        'SELECT * FROM comments left join users on users.id = comments.users_id where comments.posts_id = ? and comments.status = 1 order by comments._date desc');
         $request->execute([$id]);
         $resp = $request->fetchAll();
         return $resp;
