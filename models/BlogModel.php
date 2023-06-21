c1fb83f7c86bafc573cd4bd99744542961079d2c
kseb49 <kseb49@gmail.com>
2023-06-21 16:33:36 +0200

    add the home method

--- models/BlogModel.php
+++ models/BlogModel.php
@@ -25,7 +25,12 @@ class BlogModel
         }
         throw new Exception("Aucun article n'a été trouvé :( ");
     }
-    
+    /**
+     * Undocumented function
+     *
+     * @param string $id
+     * @return array
+     */
     public function single(string $id):array{
 
         $request = $this->db->prepare('SELECT * FROM posts p where p.id = ?');
@@ -34,6 +39,14 @@ class BlogModel
             return $resp;
         }
         throw new Exception("Cet article n'existe pas :(");
+    }
+
+    public function home():array{
+        if($request = $this->db->query('SELECT * FROM posts order by created_at desc limit 3')->fetchAll()){
+
+            return $request;
+        }
+        throw new Exception('Impossible de récupérer les articles');
     }
 }
          
