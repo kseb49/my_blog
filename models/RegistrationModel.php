da847b23198e8a356e476d62bc568e6a66773076
kseb49 <kseb49@gmail.com>
2023-06-22 17:37:55 +0200

    created a registration model

---
+++ models/RegistrationModel.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace models;
+
+use core\Model;
+
+class RegistrationModel extends Model
+{
+    protected string $f_name;
+    protected string $l_name;
+    protected string $pseudo;
+    protected string $email;
+    protected string $password;
+    protected string $confirm_password;
+
+    
+        
+    
+
+}
