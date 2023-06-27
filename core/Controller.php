000e7858d46853aa9c40218da97f8decd4e9b8b0
kseb49 <kseb49@gmail.com>
2023-06-27 17:25:38 +0200

    add redirect method to send header

--- core/Controller.php
+++ core/Controller.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace core;
 
 
@@ -8,16 +9,33 @@ abstract class Controller
 {
     private $loader;
     protected $twig;
-    
+    protected $mail_id;
+    protected $host;
+    protected $password;
 
     public function __construct()
     {
-        $this->loader = new \Twig\Loader\FilesystemLoader(ROOT.'\views');
+        $this->loader = new \Twig\Loader\FilesystemLoader(ROOT . '\views');
         $this->twig = new \Twig\Environment($this->loader, [
-        'cache' => false,
+            'cache' => false,
         ]);
         $this->twig->addExtension(new IntlExtension());
-    
+        $this->twig->addGlobal('session', $_SESSION);
+        if (file_exists(PARAMS)) {
+            $datas = json_decode(file_get_contents(PARAMS));
+            define('BASE', $datas->base_url);
+            $this->mail_id = $datas->mail->user_name;
+            $this->password = $datas->mail->password;
+            $this->host = $datas->mail->host;
+        }
     }
 
+    public function redirect(string $location ="")
+    {
+        if (!$location) {
+            header("Location: " . BASE);
+        }
+        header("Location: " . BASE . $location);
+        die();
+    }
 }
