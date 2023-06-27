3f068c8c9837ecd9553d59eb01ccfa1ce7f7dab0
kseb49 <kseb49@gmail.com>
2023-06-27 11:37:23 +0200

    add validateFromMail

--- controllers/RegisterUserController.php
+++ controllers/RegisterUserController.php
@@ -12,31 +12,40 @@ class RegisterUserController extends Controller
 
     public function form()
     {
+        if(isset($_SESSION['user'])  && !empty($_SESSION['user'])) {
+            header("Location: ".BASE);
+            die();
+        }
         $this->twig->display('registration.twig');
     }
 
     public function register()
     {
         try{
-
             if (isset($_POST) && !empty($_POST)){
                 $this->datas = $_POST;
                 $register = new RegisterUserModel();
                 if($register->loadDatas($this->datas)->validate()){
                    if($register->registerUser()){
-                    $register->mail($register->link);
+                        if($register->mail()){
+                            $this->twig->display('registration.twig');
+                        }
                    };
                 };
             }
             throw new Exception("Tous les champs doivent être remplis");
-            die();
-           
         }catch(Exception $e){
             echo $e->getMessage();
         }
+    }
 
-
-
-
+    public function validateFromMail(array $datas){
+        $this->datas = $datas;
+        $register = new RegisterUserModel();
+        if($register->confirm($datas)) {
+            session_start();
+            $_SESSION['user'] = 'auth';
+            $this->twig->display('dashboard');
+        };
     }
 }
