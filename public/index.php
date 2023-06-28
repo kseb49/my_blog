f5a7ddaec2afb77758f2d5c2755b090111f82269
kseb49 <kseb49@gmail.com>
2023-06-28 06:59:49 +0200

    new routes ans start session()

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+  session_start();
+}
 define('ROOT',dirname(__DIR__));
 define('PARAMS',dirname(__DIR__).'/env.dev.json');
 require(ROOT.'/vendor/autoload.php');
@@ -7,15 +10,21 @@ use \core\Router;
 
 try{
    $router = new Router($_SERVER['REQUEST_URI']);
+
    $router->get('/',['HomeController','index']);
    $router->get('/blog',['BlogController','index']);
    $router->get('/blog/:{id}',['BlogController','single']);
    $router->get('/blog/:{cat}/:{id}',['BlogController','group']);
    $router->get('/commentaire',['CommentController','edit']);
+
    $router->get('/inscription',['RegisterUserController','form']);
    $router->post('/inscription',['RegisterUserController','register']);
    $router->get('/validation-mail',['RegisterUserController','validateFromMail']);
-  //  $router->get('/inscription',['RegisterUserController','mailconfirmation']);
+
+   $router->post('/connexion',['UserController','logIn']);
+   $router->get('/deconnexion',['UserController','logOut']);
+   $router->get('/dashboard',['UserController','dashboard']);
+
    $router->find();
 
 }
