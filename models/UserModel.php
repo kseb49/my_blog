e3ba9f2c0ef787cc0f83b801f96202f586b899a5
kseb49 <kseb49@gmail.com>
2023-06-29 19:11:17 +0200

    move the login verification from here to the controller

--- models/UserModel.php
+++ models/UserModel.php
@@ -12,18 +12,9 @@ class UserModel extends Model{
         $request = $this->connect()->prepare('SELECT * from users where pseudo = ?');
         $request->execute([$datas['pseudo']]);
         if($user = $request->fetch()) {
-            if ($user['confirmation_date'] === null){
-                $_SESSION['error']  = ['connexion' => "le compte n'est pas confirmé"];
-                return false;
-            }
-            if(password_verify($datas['password'],$user['password'])) {
-                $this->user = $user;
-                return true;
-            }
-            $_SESSION['error']  = ['connexion' => 'pseudo ou mot de passe incorrects'];
-            return false;
+            $this->user = $user;
+            return true;
         }
-        $_SESSION['error']  = ['connexion' => 'pseudo ou mot de passe incorrects'];
         return false;
     }
 }
