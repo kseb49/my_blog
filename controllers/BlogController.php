08a66a143296b550eb9e2a26569e69fc31320456
kseb49 <kseb49@gmail.com>
2023-07-04 17:12:39 +0200

    change the type of expected params in single method

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -10,14 +10,14 @@ class BlogController extends Controller{
     public function index(){
       $datas = new BlogModel();
       $datas = $datas->index();
-      $this->twig->display('blog.twig',['datas' => $datas]);
+      return $this->twig->display('blog.twig',['datas' => $datas]);
     }
 
-    public function single(array $params){
+    public function single(string $params){
       $datas = new BlogModel();
-      $datas = $datas->single($params[0]);
+      $datas = $datas->single($params);
       $comments = new Commentmodel();
-      $comments = $comments->fetch($params[0]);
-      $this->twig->display('post.twig',['datas' => $datas,'comments'=>$comments]);
+      $comments = $comments->fetch($params);
+      return $this->twig->display('post.twig',['datas' => $datas,'comments'=>$comments]);
     }
 }
