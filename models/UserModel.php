fda847e1d809370983b6cc68772adcbee43866d4
kseb49 <kseb49@gmail.com>
2023-07-10 19:11:38 +0200

    adding user and users method

--- models/UserModel.php
+++ models/UserModel.php
@@ -9,6 +9,12 @@ class UserModel extends Model{
     public array $user;
     public array $user_post;
 
+    /**
+     * Check if a user exists
+     *
+     * @param array $datas
+     * @return boolean
+     */
     public function check(array $datas) : bool {
         $request = $this->connect()->prepare('SELECT * from users where pseudo = ?');
         $request->execute([$datas['pseudo']]);
@@ -19,10 +25,18 @@ class UserModel extends Model{
         return false;
     }
 
-    public function fetch(){
-        $request = $this->connect()->query('SELECT * from posts where users_id = '.$_SESSION['user']['id'].'');
-        if($post = $request->fetchAll()){
-            $this->user_post = $post;
+    public function user(string $id) :bool {
+        $request = $this->connect()->prepare('SELECT * from users where id = ?');
+        $request->execute([$id]);
+        if($this->user = $request->fetch()){
+            return true;
+        }
+        return false;
+    }
+
+    public function users() :bool {
+        $request = $this->connect()->query('SELECT * from users');
+        if($this->user = $request->fetchAll()){
             return true;
         }
         return false;
