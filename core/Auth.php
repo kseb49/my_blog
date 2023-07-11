c7ae4791d7c11011e9b0b3d814d7215f75f699cd
kseb49 <kseb49@gmail.com>
2023-07-11 09:34:25 +0200

    adding comments

--- core/Auth.php
+++ core/Auth.php
@@ -4,6 +4,12 @@ namespace core;
 
 final class Auth
 {
+    /**
+     * Check the permission
+     *
+     * @param integer $role
+     * @return boolean
+     */
     public static function isAuthorize(int $role):bool {
 
         if(isset($_SESSION['user'])) {
@@ -14,11 +20,21 @@ final class Auth
         }
         return false;
     }
-
+    /**
+     * Confirmed a role
+     *
+     * @param integer $role
+     * @return boolean
+     */
     public static function getRole(int $role) :bool{
         return $_SESSION['user']['role'] == $role;
     }
 
+    /**
+     * Check for an active connexion
+     *
+     * @return boolean
+     */
     public static function isConnect() :bool{
         $connect = isset($_SESSION['user']) ?? false;
         return $connect;
